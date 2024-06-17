bf0c666095465c97739823440c37c7b8cef83222
Obaid-ur-Rehman <obaidurrehman555@gmail.com>
2024-06-17 23:25:41 +0500

    making first auth screen

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from "../../constants"
+import FormField from '@/components/FormField'
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+        <View className='w-full justify-center h-full px-4 my-6'>
+          <Image
+            source={images.logo}
+            resizeMode='contain'
+            className='w-[150px] h-[35px]'
+          />
+          <Text className='text-2xl text-white font-psemibold mt-10'>Login to Aura</Text>
+          <FormField/>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
